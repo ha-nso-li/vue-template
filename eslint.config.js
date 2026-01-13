1d58c99b962e2afbb008cfb1dd12d382808880cd
정한솔 <hansoli@live.co.kr>
2026-01-13 10:50:46 +0900

    `typescriptEslint.config` was deprecated

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,11 @@
 import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import eslintPluginVue from 'eslint-plugin-vue';
+import { defineConfig } from 'eslint/config';
 import globals from 'globals';
 import typescriptEslint from 'typescript-eslint';
 
-export default typescriptEslint.config(
+export default defineConfig(
   {
     extends: [
       eslint.configs.recommended,
